854988a86914f688cb9b400bbf6255e7b37f41b4
Augusto <augustoamaro@icloud.com>
2022-03-16 22:30:19 -0300

    creating card

--- study-card/src/components/Card.js
+++ study-card/src/components/Card.js
@@ -1,7 +1,13 @@
+import styles from '../styles/components/card.module.scss';
+
 const Card = () => {
     return (
-        <div>
-            <h3>Card</h3>
+        <div className={styles["card"]}>
+            <h4>Valores e Variáveis</h4>
+
+            <h2>Introdução a tipos primitivos</h2>
+            <h2>var, let e const</h2>
+            <h2>Operador unário</h2>
         </div>
     )
 }
--- study-card/src/pages/Main.js
+++ study-card/src/pages/Main.js
@@ -1,9 +1,32 @@
+import Card from '../components/Card';
+
 import styles from "../styles/pages/main.module.scss"
 
 const Main = () => {
+    const todayYear = new Date().getFullYear();
+    const age = todayYear - 1992;
+
     return (
         <div className={styles["container"]}>
-            <h1>Hello World</h1>
+            <div className={styles["name"]}>
+                <h1>Leônidas Augusto Amaro</h1>
+                <p>Software Engineer</p>
+            </div>
+
+            <div className={styles["social-icons"]}>
+                <h5>Icon github</h5>
+                <h5>Icon linkedin</h5>
+                <h5>Icon instagram</h5>
+            </div>
+
+            <div className={styles["about"]}>
+                <h2>About me</h2>
+                <p>Hi! I'm Leônidas, a {age} yo software engineer. This project is about JavaScript concepts using cards and made with React.</p>
+            </div>
+
+            <div className={styles["card-container"]}>
+                <Card />
+            </div>
         </div>
     )
 }
